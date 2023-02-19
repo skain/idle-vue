44ef8b62228b10658498fbd4b1e33310aa065819
skain <stevekain@gmail.com>
2023-02-19 14:15:24 -0600

    Basic stuff ported over

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import IncrGame from './incr.js'
 </script>
 
 <template>
-  <header>
+<!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -14,34 +15,54 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
+  	<div class="container">
+  		<div class="row">
+  			<div class="col">
+  				<h1>Score: <span id='score'>{{ game.curPoints }}</span></h1>
+  			</div>
+  		</div>
+  		<div class="row">
+  			<div class="col">
+  				<button type="button" class="btn btn-success btn-lg" @click="game.registerClick()">Click!</button>
+  			</div>
+  		</div>
+  		<div v-if="game.curPoints > 10" class="row">
+  			<div class="col">
+  				div
+  			</div>
+  			<div class="col">
+  				div
+  			</div>
+  		</div>  		
+  	</div>
+	<footer class="footer mt-auto py-3 bg-dark">
+	  <div class="container">
+	    <div class="row">
+	  			<div class="col">
+	  				<button type="button" class="btn btn-info" @click="game.start()">Start</button>
+	  				<button type="button" class="btn btn-info" @click="game.stop()">Stop</button>
+	  				<button type="button" class="btn btn-info" @click="game.reset()">Reset</button>
+	  				<button type="button" class="btn btn-info" @click="game.resetTimer()">Reset Just Timer</button>
+	  			</div>
+	  		</div>
+	  </div>
+	</footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
+
+<script>
+export default {
+	data() {
+		return {
+			game: new IncrGame()
+		}
+	},
+	mounted() {
+		this.game.start();
+	}
+}
+</script>
