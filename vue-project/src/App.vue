be1d3450c63c2989dcbe842f82ba76b7b95c5beb
skain <stevekain@gmail.com>
2023-04-02 16:05:29 -0500

    bootstrap mostly gone

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,45 +2,23 @@
 import { reactive, onMounted } from 'vue'
 import IncrGame from './IncrGame/incrGame.js'
 import AutoClicker from './components/AutoClicker.vue'
+
+
+const game = reactive(new IncrGame());
+
+onMounted(() => {
+	game.start();
+});
 </script>
 
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<h1>Score: <span id='score'>{{ game.curPoints }}</span></h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">
-				<button type="button" class="btn btn-success btn-lg" @click="game.registerClick()">Click!</button>
-			</div>
-		</div>
-		<AutoClicker :game="game"></AutoClicker>
-	</div>
-	<footer class="">
+	<h1>Score: <span id='score'>{{ game.curPoints }}</span></h1>
+	<button type="button" class="btn btn-success btn-lg" @click="game.registerClick()">Click!</button>
+	<AutoClicker :game="game"></AutoClicker>
+	<footer>
 		<button type="button" class="btn btn-info" @click="game.start()">Start</button>
 		<button type="button" class="btn btn-info" @click="game.stop()">Stop</button>
 		<button type="button" class="btn btn-info" @click="game.reset()">Reset</button>
 		<button type="button" class="btn btn-info" @click="game.resetTimer()">Reset Just Timer</button>
 	</footer>
 </template>
-
-<script>
-// export default {
-// 	data() {
-// 		return {
-// 			game: new IncrGame()
-// 		}
-// 	},
-// 	mounted() {
-// 		this.game.start();
-// 	}
-// }
-
-const game = reactive(new IncrGame());
-onMounted(() => { 
-	alert('here');
-	game.start();
-});
-</script>
